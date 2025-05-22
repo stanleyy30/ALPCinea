a4c21be89c18bb16dca9aa494d6a150166943ca7
bryananthony <stanleyyoung300705@gmail.com>
2025-05-22 12:55:10 +0700

    push pertama

---
+++ ALP_Cinea/ALP_Cinea/ALP_CineaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_CineaApp.swift
+//  ALP_Cinea
+//
+//  Created by student on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ALP_CineaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
